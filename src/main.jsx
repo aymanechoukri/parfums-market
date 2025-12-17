263def5642c728cf2d37f29646d5761417cdcd39
aymanechoukri <aymanechoukri@example.com>
2025-12-17 14:45:48 +0100

    fix: correct vite base for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './styles.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import UsersProvider from './Context/UsersProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <UsersProvider>
         <App />
       </UsersProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
